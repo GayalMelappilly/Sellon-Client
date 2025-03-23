2d415fb9d588a49f720151ff072fe8d74c0857d3
Gayal Melappilly <gayalsunil@gmail.com>
2025-03-24 02:55:23 +0530

    feat(categories): add category tab

--- app/components/Products/Categories.tsx
+++ app/components/Products/Categories.tsx
@@ -2,27 +2,29 @@
 
 import { getAllCategories } from '@/pages/api/products/action'
 import { useQuery } from '@tanstack/react-query'
-import { CircleArrowLeft, CircleArrowRight } from "lucide-react";
+import { ArrowUp, ArrowDown } from "lucide-react";
 import React, { useEffect, useRef, useState } from 'react'
 
 type Props = {}
 
 const Categories = (props: Props) => {
 
-    const [categories, setCategories] = useState<string[]>()
+    const [allCategories, setAllCategories] = useState<string[]>()
+    const [categories, setCategories] = useState<string[]>();
+    const [show, setShow] = useState<boolean>(false)
     const scrollRef = useRef<HTMLDivElement>(null);
 
     const scroll = (direction: "left" | "right") => {
         if (scrollRef.current) {
-          const categoryWidth =
-            scrollRef.current.firstElementChild?.getBoundingClientRect().width || 0;
-    
-          scrollRef.current.scrollBy({
-            left: direction === "right" ? categoryWidth + 16 : -(categoryWidth + 16), // Adding gap size
-            behavior: "smooth",
-          });
+            const categoryWidth =
+                scrollRef.current.firstElementChild?.getBoundingClientRect().width || 0;
+
+            scrollRef.current.scrollBy({
+                left: direction === "right" ? categoryWidth + 4 : -(categoryWidth + 4), // Adding gap size
+                behavior: "smooth",
+            });
         }
-      };
+    };
 
     const { data, isLoading, isError } = useQuery({
         queryKey: ['get-categories'],
@@ -32,27 +34,32 @@ const Categories = (props: Props) => {
     useEffect(() => {
         if (data && data.categories) {
             console.log("CATEGORIES  : ", data)
-            setCategories(data.categories)
+            setAllCategories(data.categories)
+            setCategories(data.categories.slice(0,5))
         }
     }, [data])
 
+    const HandleShow = () => {
+        setShow(!show)
+        if(show){
+            setCategories(allCategories?.slice(0,5))
+        }else{
+            setCategories(allCategories)
+        }
+    }
+
     return (
-        <div className="relative mx-36">
-            <button onClick={() => scroll("left")} className="absolute left-0 top-1/2 -translate-y-1/2 bg-gray-300 p-2 rounded-full shadow-md">
-                <CircleArrowLeft />
-            </button>
-
-            <div ref={scrollRef} className="flex gap-4 mt-4 overflow-hidden scroll-smooth mx-14">
-                {categories && categories.map((category: any) => (
-                    <p key={category.name} className="rounded-md bg-sky-300/20 p-2 h-fit min-w-28 text-center whitespace-nowrap cursor-pointer">
-                        {category.name}
-                    </p>
-                ))}
+        <div className={`relative px-36 pt-8 w ${show ? 'bg-black/5' : null}`}>
+            <div ref={scrollRef} className="grid grid-cols-5 gap-4 mx-14 rounded-md">
+                {categories &&
+                    categories.map((category: any) => (
+                        <p key={category.name} className="rounded-md bg-sky-300/20 p-2 h-fit hover:scale-105 duration-300 min-w-40 text-center whitespace-nowrap cursor-pointer">
+                            {category.name}
+                        </p>
+                    ))
+                }
+                <p className='cursor-pointer absolute right-36 bottom-0 bg-sky-300/20 p-2 w-fit rounded-md' onClick={HandleShow}>{show ? <ArrowUp/> : <ArrowDown/>}</p>
             </div>
-
-            <button onClick={() => scroll("right")} className="absolute right-0 top-1/2 -translate-y-1/2 bg-gray-300 p-2 rounded-full shadow-md">
-                <CircleArrowRight />
-            </button>
         </div>
     )
 }
